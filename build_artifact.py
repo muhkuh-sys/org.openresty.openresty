085e3ae0ada43a3475ae0cb89c53117aa2db6ca5
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2023-08-25 15:38:27 +0200

    Install libcrypt for riscv64 cross-builds.

--- build_artifact.py
+++ build_artifact.py
@@ -105,6 +105,32 @@ if tPlatform['host_distribution_id'] == 'ubuntu':
         elif tPlatform['cpu_architecture'] == 'riscv64':
             # Build on linux for riscv64.
 
+            # Install the build dependencies.
+            astrDeb = [
+                'libcrypt1:riscv64',
+                'libcrypt-dev:riscv64'
+            ]
+            install.install_foreign_debs(astrDeb, strCfg_workingFolder, strCfg_projectFolder)
+
+            # This is terrible, but there was no way to pass the include and library paths as parameters to the openresty
+            # build scripts.
+            if os.path.exists('/usr/riscv64-linux-gnu/lib/libcrypt.a') is not True:
+                os.symlink(
+                    os.path.join(
+                        strCfg_workingFolder,
+                        'packages/usr/lib/riscv64-linux-gnu/libcrypt.a'
+                    ),
+                    '/usr/riscv64-linux-gnu/lib/libcrypt.a'
+                )
+            if os.path.exists('/usr/riscv64-linux-gnu/include/crypt.h') is not True:
+                os.symlink(
+                    os.path.join(
+                        strCfg_workingFolder,
+                        'packages/usr/include/crypt.h'
+                    ),
+                    '/usr/riscv64-linux-gnu/include/crypt.h'
+                )
+
             astrCMAKE_COMPILER = [
                 '-DCMAKE_TOOLCHAIN_FILE=%s/cmake/toolchainfiles/toolchain_ubuntu_riscv64.cmake' % strCfg_projectFolder
             ]
